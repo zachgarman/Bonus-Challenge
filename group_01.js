a21408e2aa456876374334372a36842dba8283df
Zach Garman <zachgarman@Zachs-MacBook-Pro.local>
2016-09-22 14:02:12 -0500

    Add comments

--- group_01.js
+++ group_01.js
@@ -6,7 +6,8 @@ var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
 
-
+//For loop cycles through each employee's array inside the
+//provided array 'employees'
 var companyArray =[];
 for (var i = 0; i < employees.length; i++) {
   companyArray.push(bonus(employees[i]));
@@ -18,49 +19,55 @@ for (var i = 0; i < employees.length; i++) {
 
 function bonus (ind) {
   var newArray = [];
-  newArray.push(ind[0]);                              //ind name
-  newArray.push((bonusCalc(ind) * 100) + '%');         //bonus percentage
+//First index of array is the employee's name.
+  newArray.push(ind[0]);
+//Second index is the percentage of their bonus as a string
+  newArray.push((bonusCalc(ind) * 100) + '%');
 
   var salary = parseInt(ind[2]);
   var totalComp = salary + salary * bonusCalc(ind);
-
-  newArray.push(totalComp);                           //total compensation
+//Third index is the employee's total compensation as a number.
+  newArray.push(totalComp);
   var totalBonus = totalComp - salary;
   totalBonus = Math.round(totalBonus);
-  newArray.push(totalBonus);                          //total Bonus amount
+//Fourth index is the total bonus amount for the employee.
+  newArray.push(totalBonus);
   console.log(newArray);
   return newArray;
 }
 
-//calculates the bonus as a decimal and returns it.
+//Calculates the bonus as a decimal and returns it.
 function bonusCalc (ind) {
   var rating = ind[3];
   var bonusPer = 0;
   var salary = parseInt(ind[2]);
 
+//Switch calculates bonus based on their rating.
   switch (rating) {
     case 5:
-      bonusPer = .10;
+      bonusPer = 0.10;
       break;
     case 4:
-      bonusPer = .06;
+      bonusPer = 0.06;
       break;
     case 3:
-      bonusPer = .04;
+      bonusPer = 0.04;
       break;
     default:
       bonusPer = 0;
       break;
   }
-
+//If employee ID is 4 numbers or less, they have worked at
+//the company for at least 15 years.  They get a 5% bonus.
   if (ind[1].length <= 4) {
     bonusPer += 0.05;
   }
-
+//If the employee already makes more than $65,000, they get
+//shafted by 1% of their bonus.
   if (salary > 65000) {
     bonusPer -= 0.01;
   }
-
+//No employee can receive a bonus of more than 13%.
   if (bonusPer > 0.13) {
     bonusPer = 0.13;
   }
